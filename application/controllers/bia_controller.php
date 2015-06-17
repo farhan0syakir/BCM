3b8f4fc66c0a8d4bdbb702b0bad9d82482787a99
Farhan Syakir <farhan0syakir@gmail.com>
2015-06-17 12:52:54 +0700

    semakin clean

--- application/controllers/bia_controller.php
+++ application/controllers/bia_controller.php
@@ -5,9 +5,17 @@ class Bia_controller extends CI_Controller {
 		$bia = array	(
 			array("id"=>"1" ,"name"=>"BA 1"),
 			array("id"=>"2" ,"name"=>"BA 2"),
+
+			array("id"=>"12" ,"name"=>"BA 232"),
+			array("id"=>"24" ,"name"=>"BA 1233"),
+			array("id"=>"21" ,"name"=>"BAv 11"),
+			array("id"=>"233" ,"name"=>"BA 22"),
+			array("id"=>"21" ,"name"=>"BA 1111"),
 			array("id"=>"3" ,"name"=>"BA 3")
 		);
-		$this->load->template('index',$bia);
+		$data['bia'] = $bia;
+		// $this->load->template('layout/content',$bia);
+		$this->load->template('pages/bia/index',$data);
 	}
 
 	public function view($id){
--- application/controllers/home_controller.php
+++ application/controllers/home_controller.php
@@ -3,15 +3,8 @@ class Home_controller extends CI_Controller {
 
 	public function index()
 	{
-		$bia = array	(
-			array("id"=>"1" ,"name"=>"BA 1"),
-			array("id"=>"2" ,"name"=>"BA 2"),
-			array("id"=>"3" ,"name"=>"BA 3")
-		);
-		$data['bia'] = $bia;
-		// $this->load->template('layout/content',$bia);
-		$this->load->template('pages/bia/index',$data);
-
+		$this->load->library('../controllers/bia_controller');
+		$this->bia_controller->index();
 	}
 
 	public function comments()

2eb1e8340ca71d98b1385e77b9120ecf6ac733a1
Farhan Syakir <farhan0syakir@gmail.com>
2015-06-18 14:58:06 +0700

    pembersihan file

--- application/views/pages/ra/form.php
+++ application/views/pages/ra/form.php
@@ -5,14 +5,14 @@
 //todo silahkan buat form disini
 echo form_open('ra_controller/add');
 echo form_label('threat')."</br>";
-echo form_input('threat')."</br>";
+echo form_input(array('id'=>'threat','required'=>'true'))."</br>";
 echo form_label('P')."</br>";
-echo form_input('P')."</br>";
+echo form_input(array('id'=>'P','required'=>'true'))."</br>";
 echo form_label('I')."</br>";
-echo form_input('I')."</br>";
+echo form_input(array('id'=>'I','required'=>'true'))."</br>";
 echo form_label('Pm')."</br>";
-echo form_input('Pm')."</br>";
+echo form_input(array('id'=>'Pm','required'=>'true'))."</br>";
 echo form_label('Im')."</br>";
-echo form_input('Im')."</br>";
+echo form_input(array('id'=>'Im','required'=>'true'))."</br>";
 echo form_submit('mysubmit', 'Submit Post!');
 echo form_close();

c54610d887f7cf4689cd6e4dcfb02799e1b2b88b
Azka Nurun Ala <nurunalaazka@gmail.com>
2015-06-24 11:56:12 +0700

    Delete confirmation added

--- application/views/pages/bia/index.php
+++ application/views/pages/bia/index.php
@@ -41,7 +41,7 @@
 										   View <span class="glyphicon glyphicon-check" aria-hidden="true"></span></a></td>
 											<td  class="td-button text-center"><a class="edit-button-gap crud" href="<?php echo base_url('bia/edit/').'/'.$bia[$i]->id;?>">
 											Edit <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a></td>
-											<td  class="td-button text-right"><a class="delete-button-gap crud" href="<?php echo base_url('bia/delete/').'/'.$bia[$i]->id;?>">
+											<td  class="td-button text-right"><a class="delete-button-gap crud" data-toggle="modal" data-target="#delete" href="#" >
 											Delete <span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a></td>
 										</tr>
 										<?php endfor; ?>
@@ -56,6 +56,38 @@
                     <!-- /.panel -->
                 </div>
 			</div>
+
+            <div id="delete" class="modal fade" role="dialog">
+                  <div class="top-padding modal-dialog text-center">
+
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title ">Delete Confirmation</h4>
+                      </div>
+                      <div class="modal-body">
+                        <p>'Are you sure to delete Liquidity Management MM' ?</p>
+                      </div>
+                      <div class="bottom-gap modal-body">
+                        <div class="row"> 
+                           
+                            <div class="padding-right-narrow text-right col-lg-6 ">
+                                <a class="button-gap crud" data-dismiss="modal" href="#">
+                                <span class="glyphicon glyphicon-remove"  aria-hidden="true"></span> CANCEL</a>
+                            </div>
+                             <div class="padding-leftt-narrow text-left col-lg-6">
+                                <a class="delete-button-gap crud" data-dismiss="modal" href="#">
+                                DELETE <span class="glyphicon glyphicon-ok"  aria-hidden="true"></span></a>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+
+                  </div>
+          </div>
+
+
 		
 		<!--div class="row">
                 <div class=" col-lg-12">

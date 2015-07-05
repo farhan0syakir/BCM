2c4ebb41d1b241ce36116b7474914f1c2427cb02
Azka Nurun Ala <nurunalaazka@gmail.com>
2015-07-06 06:29:26 +0700

    edit UI login

--- application/views/pages/login/index.php
+++ application/views/pages/login/index.php
@@ -1,14 +1,15 @@
 <div class="top-padding text-center">
+		<h4>Login to Your Account</h4>
 		<div class="form-login">
 			<?php echo form_open("login/process")?>
 				<div class="form-group" method="post">
-					<input name="username" type ="username" class="form-control" placeholder="Username">
-						
+					<input  name="username" type ="username" class="top-gap form-control" placeholder="Username">
+				</div>
 				<div class="form-group"  method="post">
-					<input name="password" type="password" class="form-control" placeholder="Password">
-				
-				<div class="form-group text-center">
-					<input name="is_remember_me" type="checkbox" value="true">Remember  me
+					<input  name="password" type="password" class="top-gap-narrow form-control" placeholder="Password">
+				</div>
+				<div class="top-gap-narrow form-group text-left">
+					<input name="is_remember_me" class="top-gap" type="checkbox" value="true"> Remember  me
 				</div>
 						
 			
@@ -17,15 +18,16 @@
 					<div class="row"> 
 							<div class="padding-leftt-narrow text-left col-lg-12">
 									<div class="col-lg-12 text-center">
-										<button type = "submit" class="button-gap">Login
-											<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-										</button>
+										<a type = "submit" class="button-gap">Login 
+											<span class="left-gap glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
+										</a>
 									</div>
 								</div>
 							</div>
 						</div>
                  
 				</div>
+</div>
 				<?php if(!empty($error_message)){
 					echo $error_message;
 				}?>

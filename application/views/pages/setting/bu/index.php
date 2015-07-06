840732ae840a1808d78cf6827923da45056b953e
Azka Nurun Ala <nurunalaazka@gmail.com>
2015-07-06 13:47:39 +0700

    UI designer

--- application/views/pages/setting/bu/index.php
+++ application/views/pages/setting/bu/index.php
@@ -14,7 +14,7 @@
 				<div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            
+                            <h4>List of Business Units</h4>
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">

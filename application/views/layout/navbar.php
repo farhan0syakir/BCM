f805eeb86524258624131e0c5015d935b0170a32
Azka Nurun Ala <nurunalaazka@gmail.com>
2015-07-07 16:32:01 +0700

    navbar modif

--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -8,7 +8,6 @@
                 <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand page-scroll" href="index.php#page-top">
-			<img src="<?php echo base_url('assets/images/logo.png')?>"/>
 			<img class="logo" src="<?php echo base_url('assets/images/logo.png')?>"/>
 			</a>
         </div>
@@ -21,12 +20,6 @@
                     <a href="#"><img class="notif" src="<?php echo base_url('assets/images/approve.png')?>"></a>
                 </li>
                 <ul class="dropdown-menu" >
-					    <li class="text-right"><a href="#">Business Activity TCM-006 you created has been approved<br>
-					    <p style="font-size:10pt;color:orange;" href="#">View</p></a></li>
-					     <li class="text-right"><a href="#">Business Activity TCM-005 you created has been approved<br>
-					    <p style="font-size:10pt;color:orange;" href="#">View</p></a></li>
-					     <li class="text-right"><a href="#">Business Activity TCM-004 you created has been approved<br>
-					    <p style="font-size:10pt;color:orange;" href="#">View</p></a></li>
 					    <li class="text-left"><a href="#">Business Activity TCM-006 you created has been approved<br>
 					    <p class="text-right" style="font-size:10pt;color:orange;" href="#">View</p></a></li>
 					     <li class="text-left"><a href="#">Business Activity TCM-005 you created has been approved<br>
@@ -34,7 +27,6 @@
 					     <li class="text-left"><a href="#">Business Activity TCM-004 you created has been approved<br>
 					    <p class="text-right" style="font-size:10pt;color:orange;" href="#">View</p></a></li>
 					    <li role="separator" class="divider"></li>
-					    <li class="text-center"><a href="#">List of Approvals <span class="left-gap glyphicon glyphicon-option-horizontal" aria-hidden="true"></span> </a></li>
 					    <li class="text-center"><a href="#">List of Approvals<span class="left-gap glyphicon glyphicon-option-horizontal" aria-hidden="true"></span> </a></li>
 				</ul>
 				</div>
@@ -44,12 +36,6 @@
 	                    <a class="no-padding " href="#"><img class="notif" src="<?php echo base_url('assets/images/notif.png')?>"></a>
 	                </li>
 	                <ul class="dropdown-menu" >
-					    <li class="text-right"><a href="#">SOP for Fire has been activated<br>
-					    <p style="font-size:10pt;color:orange;" href="#">See instructions</p></a></li>
-					    <li class="text-right"><a href="#">SOP for Flood has been activated<br>
-					    <p style="font-size:10pt;color:orange;" href="#">See instructions</p></a></li>
-					    <li class="text-right"><a href="#">SOP for System Down has been activated<br>
-					    <p style="font-size:10pt;color:orange;" href="#">See instructions</p></a></li>
 					    <li class="text-left"><a href="#">SOP for Fire has been activated<br>
 					    <p class="text-right" style="font-size:10pt;color:orange;" href="#">See instructions</p></a></li>
 					    <li class="text-left"><a href="#">SOP for Flood has been activated<br>
@@ -57,7 +43,6 @@
 					    <li class="text-left"><a href="#">SOP for System Down has been activated<br>
 					    <p class="text-right" style="font-size:10pt;color:orange;" href="#">See instructions</p></a></li>
 					    <li role="separator" class="divider"></li>
-					    <li class="text-center"><a href="#">See all notifications <span class="left-gap glyphicon glyphicon-option-horizontal" aria-hidden="true"></span> </a></li>
 					    <li class="text-center"><a href="#">See all notifications<span class="left-gap glyphicon glyphicon-option-horizontal" aria-hidden="true"></span> </a></li>
 				</ul>
 				</div>
@@ -67,8 +52,6 @@
 	                    <a class="no-padding " href="#"><img class="notif" src="<?php echo base_url('assets/images/user.png')?>"></a>
 	                </li>
 	                <ul class="dropdown-menu" >
-					    <li><a href="#">Account</a></li>
-					    <li><a href="#">My Work</a></li>
 					    <li class="text-center"><a href="#">Account</a></li>
 					    <li class="text-center"><a href="#">My Work</a></li>
 					    <li role="separator" class="divider"></li>
